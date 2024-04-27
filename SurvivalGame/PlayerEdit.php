d6821084fbf04716492f947e60811d88d939df64
MaeenMo <2101544@eng.asu.edu.eg>
2024-04-27 21:09:16 +0300

    modified: login button color and added active effect, modified edit player button

--- SurvivalGame/PlayerEdit.php
+++ SurvivalGame/PlayerEdit.php
@@ -66,7 +66,7 @@ if (isset($_GET['PlayerID'])) {
                     <input type="number" id="zCoordinate" name="zCoordinate" value="<?php echo htmlspecialchars($player['ZCoordinate']); ?>" required>
                 </div>
             </div>
-            <button type="submit" name="update_player">Update Player</button>
+            <button type="submit" name="update_player" class="btn">Update Player</button>
         </form>
     <?php else: ?>
         <p>No player found.</p>
